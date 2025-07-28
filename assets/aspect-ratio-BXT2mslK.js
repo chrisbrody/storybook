5792bfaecdaa4f46f5afb149ea2ece57b07e3180
chrisbrody <chrisbrody@users.noreply.github.com>
2025-07-28 15:55:19 +0000

    deploy: 474844d72da83f9f8ad92779dabb5f77588a8b69

---
+++ assets/aspect-ratio-BXT2mslK.js
@@ -0,0 +1,6 @@
+import{c as p}from"./createLucideIcon-BOffmja0.js";import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{r as c}from"./iframe-GmFyfI-B.js";import{P as d}from"./index-DmtSfvaN.js";/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]],u=p("arrow-right",n);var m="AspectRatio",a=c.forwardRef((t,r)=>{const{ratio:i=1/1,style:e,...s}=t;return o.jsx("div",{style:{position:"relative",width:"100%",paddingBottom:`${100/i}%`},"data-radix-aspect-ratio-wrapper":"",children:o.jsx(d.div,{...s,ref:r,style:{...e,position:"absolute",top:0,right:0,bottom:0,left:0}})})});a.displayName=m;var f=a;function h({...t}){return o.jsx(f,{"data-slot":"aspect-ratio",...t})}h.__docgenInfo={description:"",methods:[],displayName:"AspectRatio"};export{u as A,h as a};
---
+++ assets/index-Cggi_Ipk.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-GmFyfI-B.js";import{r as o}from"./index-g7PYI-s1.js";var t=o();const m=r(t);export{m as P,t as r};
